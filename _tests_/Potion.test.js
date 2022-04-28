e33c187eada0eb6d90be22ac72c630ddbb54044a
woodb58 <woodb58@gmail.com>
2022-04-28 14:09:23 -0500

    player/enemy

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,13 +1,13 @@
-const Potion = require("../lib/Potion.js");
+const Potion = require('../lib/Potion.js');
 
-test("creates a health potion object", () => {
-  const potion = new Potion("health");
+test('creates a health potion object', () => {
+  const potion = new Potion('health');
 
-  expect(potion.name).toBe("health");
+  expect(potion.name).toBe('health');
   expect(potion.value).toEqual(expect.any(Number));
 });
 
-test("creates a random potion object", () => {
+test('creates a random potion object', () => {
   const potion = new Potion();
 
   expect(potion.name).toEqual(expect.any(String));
